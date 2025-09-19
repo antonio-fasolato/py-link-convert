f9eaab61454e1a588b6b338ff71fadaf7016ee17
Antonio Fasolato <antonio.fasolato@gmail.com>
2025-09-19 09:04:44 +0200

    Added parameter to start with development configuration

--- main.py
+++ main.py
@@ -8,15 +8,33 @@ from routes import health, convert, history
 from security.auth import handle_api_key
 from services import SqliteService
 
-# Configurazione del logging
+# Command line parsing
+parser = argparse.ArgumentParser(
+    prog="py-link-sender",
+    description="A script to convert links to Epub and Moby files",
+    epilog="Copyright Antonio Fasolato 2025"
+)
+parser.add_argument(
+    '-k',
+    '--create-api-key',
+    help='Create a new api key',
+)
+parser.add_argument(
+    '-d',
+    '--debug',
+    help='Start with development configuration',
+    action='store_true'
+)
+args = parser.parse_args()
+
+# Logging configuration
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.INFO if args.debug else logging.ERROR,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     handlers=[
         logging.StreamHandler()
     ]
 )
-
 logger = logging.getLogger(__name__)
 
 # Inizializzazione FastAPI
@@ -37,26 +55,14 @@ app.include_router(
 
 sqlite_service = SqliteService()
 
-parser = argparse.ArgumentParser(
-    prog="py-link-sender",
-    description="A script to convert links to Epub and Moby files",
-    epilog="Copyright Antonio Fasolato 2025"
-)
-parser.add_argument(
-    '-k',
-    '--create-api-key',
-    help='Create a new api key',
-)
-args = parser.parse_args()
-
 if __name__ == "__main__":
     if args.create_api_key:
         logger.info("Creating a new api-key")
         new_key = sqlite_service.create_new_api_key(args.create_api_key)
-        logger.info(f'Created new key: {new_key}')
+        print(f'Created new key: {new_key}')
     else:
         # Ottenimento parametri host e port dalle variabili d'ambiente
         host = os.getenv('UVICORN_HOST', '0.0.0.0')
         port = int(os.getenv('UVICORN_PORT', '8000'))
 
-        uvicorn.run("main:app", host=host, port=port, reload=True)
+        uvicorn.run("main:app", host=host, port=port, reload=True, log_level='info' if args.debug else 'error')
